4ea1059ed46c2dbc8d09a3cbb86dcb9ebc050325
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-01-30 12:04:54 +0100

    run-prom: use /data

--- run-prom.sh
+++ run-prom.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo podman run --rm -p 9090:9090 --user $(id -u) -v $(pwd)/prometheus:/prometheus-data prom/prometheus:latest --config.file=/prometheus-data/prometheus.yml --storage.tsdb.path=/prometheus-data
+sudo podman run --rm -p 9090:9090 --user $(id -u) -v $(pwd)/prometheus:/data prom/prometheus:latest --config.file=/data/prometheus.yml --storage.tsdb.path=/data
